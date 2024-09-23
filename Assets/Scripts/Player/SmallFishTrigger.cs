0d1905ec50df1dfec60a01eedf9bac3a250f81c8
JyothsnaPDS01 <jyothsna@playdgamestudio.com>
2024-09-23 16:09:08 +0530

    New changes

--- Assets/Scripts/Player/SmallFishTrigger.cs
+++ Assets/Scripts/Player/SmallFishTrigger.cs
@@ -12,14 +12,54 @@ namespace SharkGame
         [SerializeField] private Transform _sharkMouthPosition;
 
         [SerializeField] private Player _player;
+        [SerializeField] private Transform _playerShark;
 
-    
+        [SerializeField] private float detectionInterval;
+        [SerializeField] private float detectionRadius;
+        [SerializeField] private LayerMask fishLayerMask;
+
+        private void Start()
+        {
+            //StartCoroutine(CheckNearbyFishesAtIntervals());
+        }
+
+        private IEnumerator CheckNearbyFishesAtIntervals()
+        {
+            while (true) // This loop will keep running indefinitely
+            {
+                yield return new WaitForSeconds(detectionInterval); // Wait for the detection interval
+
+                // Check for nearby fishes
+                yield return StartCoroutine(CheckNearbyFishesOverTime());
+            }
+        }
+
+        private IEnumerator CheckNearbyFishesOverTime()
+        {
+            Collider[] nearbyFishes = Physics.OverlapSphere(_playerShark.position, detectionRadius, fishLayerMask);
+
+            foreach (Collider fishCollider in nearbyFishes)
+            {
+                if (fishCollider.CompareTag("SmallFish") && fishCollider.isTrigger)
+                {
+#if UNITY_EDITOR
+                    Debug.Log("Finding the small fishes");
+#endif
+                    GameObject fishObject = fishCollider.gameObject;
+                    StartCoroutine(BringSmallFishesNearToPlayer(fishObject));
+                    yield return null; // Yield after each fish to avoid performance spikes
+                }
+            }
+        }
+
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.tag == "SmallFish")
             {
+#if UNITY_EDITOR
                 Debug.Log("smallfish enters");
+#endif
                 other.gameObject.transform.SetParent(_sharkHeadPosition);
                 StartCoroutine(DeactiveSmallFishAndPushBackToPool(other.gameObject));
             }
@@ -38,7 +78,8 @@ namespace SharkGame
                 yield return null;
             }
 
-            _fishObject.transform.localPosition = _sharkMouthPosition.localPosition;
+            _fishObject.transform.position = _sharkMouthPosition.localPosition;
+
             _fishObject.transform.SetParent(_sharkMouthPosition);
 
             // Trigger shark attack animation
@@ -47,12 +88,12 @@ namespace SharkGame
             // Play blood effect
             _player.EnableBloodEffect();
 
-            yield return new WaitForSeconds(.5f);
+            yield return new WaitForSeconds(.7f);
 
             // Mark the fish as dead and reset its state
             SharkGameDataModel.SmallFishType fishType = _fishObject.GetComponent<SmallFish>()._smallFishType;
-            _fishObject.GetComponent<SmallFish>()._currentState = SharkGameDataModel.SmallFishFiniteState.Die;
-            _fishObject.GetComponent<SmallFish>().ResetFishState();
+            //_fishObject.GetComponent<SmallFish>()._currentState = SharkGameDataModel.SmallFishFiniteState.Die;
+            //_fishObject.GetComponent<SmallFish>().ResetFishState();
 
             // Return fish to the pool
             _fishObject.transform.parent = null;
@@ -66,6 +107,47 @@ namespace SharkGame
             _player.DisableBloodEffect();
         }
 
+
+        private IEnumerator BringSmallFishesNearToPlayer(GameObject _fishObject)
+        {
+            float duration = .1f;
+            float elapsedTime = 0f;
+
+            // Move the fish to the shark's mouth position
+            while (elapsedTime < duration)
+            {
+                _fishObject.transform.localPosition = Vector3.Lerp(_fishObject.transform.localPosition, _sharkMouthPosition.localPosition, elapsedTime / duration);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            _fishObject.transform.position = _sharkMouthPosition.localPosition;
+            _fishObject.transform.SetParent(_sharkMouthPosition);
+
+            // Trigger shark attack animation
+            _player.PlayEatAnimation();
+
+            // Play blood effect
+            _player.EnableBloodEffect();
+
+            yield return new WaitForSeconds(.7f);
+
+            // Mark the fish as dead and reset its state
+            SharkGameDataModel.SmallFishType fishType = _fishObject.GetComponent<SmallFish>()._smallFishType;
+            //_fishObject.GetComponent<SmallFish>()._currentState = SharkGameDataModel.SmallFishFiniteState.Die;
+            //_fishObject.GetComponent<SmallFish>().ResetFishState();
+
+            // Return fish to the pool
+            _fishObject.transform.parent = null;
+            ObjectPooling.Instance.ReturnToPool(_fishObject, fishType);
+
+            yield return new WaitForSeconds(.5f);
+
+            // Reset shark attack animation and disable blood effect
+            _player.BackToIdleAnimation();
+
+            _player.DisableBloodEffect();
+        }
         private void OnCollisionExit(Collision collision)
         {
 
